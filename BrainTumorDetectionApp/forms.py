92180be69f7e6b3ff88a0ba1baeb10e5c2d23607
unknown <braintumordetection2025@gmail.com>
2025-10-08 14:01:23 +0530

    08-10-25

--- BrainTumorDetectionApp/forms.py
+++ BrainTumorDetectionApp/forms.py
@@ -3,4 +3,30 @@ from .models import *
 class DoctorForm(forms.ModelForm):
     class Meta:
         model = DoctorTable
-        fields = ['Name','PhNumber', 'Specialization', 'HospitalName', 'Email']
+        fields = ['Name','PhNumber','Specialization','HospitalName','Email']
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = PatientTable
+        fields = ['Name','Age','Gender','PhNumber','Email','Address']
+
+class AppoinmentForms(forms.ModelForm):
+    class Meta:
+        model = AppoinmentTable
+        fields = ['Date','Status']
+
+class MedicineForms(forms.ModelForm):
+    class Meta:
+        model = MedicineTable
+        fields = ['Medicine','Description','Quantity','Price']
+
+class PrescriptionForms(forms.ModelForm):
+    class Meta:
+        model = PrescriptionTable
+        fields = ['Prescription','Medicine']
+
+class PostForms(forms.ModelForm):
+    class Meta:
+        model = PostTable
+        fields = ['Image','Document','Date']
+        
